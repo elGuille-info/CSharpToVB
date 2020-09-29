0408a7262e72ee43182e5f1c339f1165194f6f65
Paul M Cohen <paul1956@users.noreply.github.com>
2020-09-28 18:35:17 -0700

    Change design of TextProgressBar to be ToolStripTestProgressBar UserControl.

--- CSharpToVB/My Project/Application.Designer.vb
+++ CSharpToVB/My Project/Application.Designer.vb
@@ -11,48 +11,34 @@
 Option Strict On
 Option Explicit On
 
-Namespace My
 
+Namespace My
+    
     'NOTE: This file is auto-generated; do not modify it directly.  To make changes,
     ' or if you encounter build errors in this file, go to the Project Designer
     ' (go to Project Properties or double-click the My Project node in
     ' Solution Explorer), and make changes on the Application tab.
     '
     Partial Friend Class MyApplication
-#If NETCOREAPP3_1 Then
-        Inherits ApplicationServices.WindowsFormsApplicationBase
-#End If
-
-        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>
+        
+        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Public Sub New()
-#If NETCOREAPP3_1 Then
-            MyBase.New()
-#Else
             MyBase.New(Global.Microsoft.VisualBasic.ApplicationServices.AuthenticationMode.Windows)
-#End If
-            Me.EnableVisualStyles = True
             Me.IsSingleInstance = true
+            Me.EnableVisualStyles = True
+            Me.SaveMySettingsOnExit = True
             Me.MinimumSplashScreenDisplayTime = 5000
-            Me.SaveMySettingsOnExit = true
             Me.ShutDownStyle = Global.Microsoft.VisualBasic.ApplicationServices.ShutdownMode.AfterMainFormCloses
         End Sub
-
-        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>
+        
+        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-#If NETCOREAPP3_1 Then
-            Me.MainForm = New Form1
-#Else
             Me.MainForm = Global.CSharpToVBApp.Form1
-#End If
         End Sub
-
-        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>
+        
+        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateSplashScreen()
-#If NETCOREAPP3_1 Then
-            Me.SplashScreen = New SplashScreen1
-#Else
             Me.SplashScreen = Global.CSharpToVBApp.SplashScreen1
-#End If
         End Sub
     End Class
 End Namespace
